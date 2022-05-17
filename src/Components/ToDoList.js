6cba8475129c3a2cb2fd8e085d1a0b331b5dbd3a
Jacob <jacob.d.wencloff@gmail.com>
2022-05-17 15:10:07 -0700

    Created a multiLevel todo list using ReactStrap component features

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react'
-import { Table } from 'reactstrap'
+import { Table, Dropdown, DropdownItem, DropdownToggle, DropdownMenu } from 'reactstrap'
+import { isDOMComponent } from 'react-dom/test-utils';
 
 export default function ToDoList() {
     const [listItem, setNewListItem] = useState('')
-    const [list, setNewList] = useState([])
+    const [projectList, setNewProjectList] = useState([])
+    const [weeklyList, setNewWeeklyList] = useState([])
+    const [dailyList, setNewDailyList] = useState([])
+    const [ddOpen, setDDOpen] = useState(false)
+    const [entryType, setEntryType] = useState(null)
 
     const handleChange = (event) => {
         event.preventDefault()
@@ -13,34 +19,129 @@ export default function ToDoList() {
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        let listCopy = [...list]
-        listCopy.push(listItem)
-        setNewList(listCopy)
+        if(entryType === 'Projects'){
+            let projectListCopy = [...projectList]
+            projectListCopy.push(listItem)
+            setNewProjectList(projectListCopy)            
+        }else if(entryType === 'Weekly Goals'){
+            let weeklyListCopy = [...weeklyList]
+            weeklyListCopy.push(listItem)
+            setNewWeeklyList(weeklyListCopy)
+        }else if(entryType === 'Daily Tasks'){
+            let dailyListCopy = [...dailyList]
+            dailyListCopy.push(listItem)
+            setNewDailyList(dailyListCopy)
+        }
         setNewListItem('')
+
     }
 
+    const handleEntryType = (e) =>{
+        e.preventDefault()
+        let target = e.target.innerText
+        if(target === 'Projects'){
+            setEntryType('Projects')
+            console.log('project selected')
+        }else if(target === 'Weekly Goals'){
+            setEntryType('Weekly Goals')
+            console.log('weekly selected')
+        }else if(target === 'Daily Tasks'){
+            setEntryType('Daily Tasks')
+            console.log('Daily selected')
+        }
+    }
 
-    const mappedList = list.map((item, i) => {
-        return (
-            <tr>{item}</tr>
+    const mappedDailyList = dailyList.map((item, i) =>{
+        return(
+            <tr key={item + i}>
+                <td>
+                    {item}
+                </td>
+            </tr>
+        )
+    })
+    const mappedWeeklyList = weeklyList.map((item, i) =>{
+        return(
+            <tr key={item + i}>
+                <td>
+                    {item}
+                </td>
+            </tr>
+        )
+    })
+    const mappedProjectList = projectList.map((item, i) =>{
+        return(
+            <tr key={item + i}>
+                <td>
+                    {item}
+                </td>
+            </tr>
         )
     })
 
 
 
+    // const mappedList = list.map((item, i) => {
+    //     return (
+    //         <tr key={item + i}>
+    //             <td>
+    //                 {item}
+    //             </td>
+    //         </tr>
+    //     )
+    // })
+
+    const handleToggle = () =>{
+        if(ddOpen){
+            setDDOpen(false)
+        }else{
+            setDDOpen(true)
+        }
+    }
+
     return (
         <div>
             <form onSubmit={handleSubmit}>
+                <Dropdown isOpen={ddOpen} toggle={handleToggle}>
+                    <DropdownToggle caret>
+                        Select Type
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem onClick={handleEntryType}>
+                            Projects
+                        </DropdownItem>
+                        <DropdownItem onClick={handleEntryType}>
+                            Weekly Goals
+                        </DropdownItem>
+                        <DropdownItem onClick={handleEntryType}>
+                            Daily Tasks
+                        </DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
                 <input onChange={handleChange} type="text" value={listItem} placeholder='Enter New Item...'></input>
                 <input type='submit' value="submit"></input>
             </form>
-            <Table>
-                <tr>
-                    <th>Header</th>
-                </tr>
-                {mappedList}
+            <Table hover>
+                <th>
+                    Projects
+                </th>
+                <tbody>
+                    {mappedProjectList}
+                </tbody>
+                <th>
+                    Weekly Goals
+                </th>
+                <tbody>
+                    {mappedWeeklyList}
+                </tbody>
+                <th>
+                    Daily Tasks
+                </th>
+                <tbody>
+                    {mappedDailyList}
+                </tbody>
             </Table>
-           
+
         </div>
     )
 }
