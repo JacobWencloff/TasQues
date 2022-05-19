e82474b961b9e527ded14ba12f9834aac1e3bc4b
Jacob <jacob.d.wencloff@gmail.com>
2022-05-19 13:42:10 -0700

    Introduced basic arithmatic to the Calculator component

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -3,39 +3,57 @@ import { Button, Alert } from 'reactstrap'
 import { useState } from 'react'
 
 export default function Calculator() {
-  let [functionLine, setFunctionLine] = useState({
-    sign: "",
-    num: 0,
-    res: 0,
-  })
+  let [functionLine, setFunctionLine] = useState('')
+
+  let [operator, setOperator] = useState('+')
 
   const handleNum = (e) =>{
     e.preventDefault()
-    console.log()
-    
+    let requestedVal = Number(e.target.innerText)
+
+    if(functionLine === ''){
+      setFunctionLine(requestedVal)
+    }
+
+    if(operator === '+'){
+       setFunctionLine(functionLine + requestedVal)
+    }else if(operator === '-' ){
+       setFunctionLine(functionLine - requestedVal)
+    }else if(operator === 'X'){
+      setFunctionLine(functionLine * requestedVal)
+    }else if(operator === '/'){
+      setFunctionLine(functionLine / requestedVal)
+    } else{
+      console.log('please enter operator')
+    }
+    // setFunctionLine(functionLine + requestedVal)
+    // console.log(functionLine)
   }
   const handleOp = (e) =>{
-    
+    let requestedOp = e.target.innerText
+    setOperator(requestedOp)
+    console.log(operator)
   }
-
 
   return (
     <div>
       <Alert>
-      
+        {functionLine}
       </Alert>
       <div>
-        <Button onClick={handleOp}>+</Button>
-        <Button onClick={handleOp}>-</Button><br/>
+       
         <Button onClick={handleNum}>1</Button>
         <Button onClick={handleNum}>2</Button>
-        <Button onClick={handleNum}>3</Button><br/>
+        <Button onClick={handleNum}>3</Button>
+        <Button onClick={handleOp}>X</Button><br/>
         <Button onClick={handleNum}>4</Button>
         <Button onClick={handleNum}>5</Button>
-        <Button onClick={handleNum}>6</Button><br/>
+        <Button onClick={handleNum}>6</Button>
+        <Button onClick={handleOp}>/</Button><br/>
         <Button onClick={handleNum}>7</Button>
         <Button onClick={handleNum}>8</Button>
         <Button onClick={handleNum}>9</Button>
+        <Button onClick={handleOp}>+</Button>
       </div>
     </div>
   )
